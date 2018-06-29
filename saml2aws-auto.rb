7aaf52e21607bef984d3783ef82cd7dde913bb14
Daniel Stamer <daniel.stamer@cloudreach.com>
2018-06-29 10:22:48 +0200

    update hash
    Signed-off-by: Daniel Stamer <daniel.stamer@cloudreach.com>

--- saml2aws-auto.rb
+++ saml2aws-auto.rb
@@ -3,7 +3,7 @@ class Saml2awsAuto < Formula
   homepage 'https://github.com/rukenshia/saml2aws-auto'
   version '1.4.0'
   url "https://github.com/Rukenshia/saml2aws-auto/releases/download/#{version}/saml2aws-auto-#{version}-x86_64-apple-darwin.tar.gz"
-  sha256 '805bc980d71e0a03de2d08d9f1785fef9a4fd3baa9e1a26aa72d98372d671dcb'
+  sha256 'afd14a9252f7b6c94f2a6a494df31be2a6a56047784d775647933459f378b1b0'
   
   def install
     bin.install 'saml2aws-auto'  
