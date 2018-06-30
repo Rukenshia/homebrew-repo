60651b2126200db0d1b8fd1a6fafe7f732860a5a
Jan C <christophersen.jan@googlemail.com>
2018-06-30 10:33:23 +0200

    Update saml2aws-auto.rb

--- saml2aws-auto.rb
+++ saml2aws-auto.rb
@@ -3,7 +3,7 @@ class Saml2awsAuto < Formula
   homepage 'https://github.com/rukenshia/saml2aws-auto'
   version '1.4.1'
   url "https://github.com/Rukenshia/saml2aws-auto/releases/download/#{version}/saml2aws-auto-#{version}-x86_64-apple-darwin.tar.gz"
-  sha256 'c5060b12458406364920237b4464e69705d0c8b09468509860572267c8e2e37b'
+  sha256 '08cc0555875dfd466978b269eb3e0c722fe0d0185ec42185b0e4dfb50ce770ec'
   
   def install
     bin.install 'saml2aws-auto'  
