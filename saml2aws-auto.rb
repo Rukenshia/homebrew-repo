01490e55536de6172706712ef5b962bc6070fb39
Jan C <code@chrstphrsn.cc>
2020-05-29 11:25:17 +0200

    Update saml2aws-auto.rb

--- saml2aws-auto.rb
+++ saml2aws-auto.rb
@@ -1,9 +1,9 @@
 class Saml2awsAuto < Formula
   desc "Access, manage & use STS token for federated access into AWS using SAML2"
   homepage 'https://github.com/rukenshia/saml2aws-auto'
-  version '1.7.8'
+  version '1.8.0'
   url "https://github.com/Rukenshia/saml2aws-auto/releases/download/#{version}/saml2aws-auto-#{version}-x86_64-apple-darwin.tar.gz"
-  sha256 'c18f622e0d91ddf0aaf95fd3c514cd0531eab8c604ad7115e1c0c35fb02c4cab'
+  sha256 '63dd1cec7b8c5566a826f245d62587c9e07581b9f6f36397f3a474cfb053ca77'
   
   def install
     bin.install 'saml2aws-auto'  
